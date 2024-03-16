c14d3375cfd312cd599cfb3743b708901fff397b
00Jakub00 <jakubgaly77@gmail.com>
2024-03-16 20:51:02 +0100

    Starting code in Main class

--- src/main/java/org/project/Main.java
+++ src/main/java/org/project/Main.java
@@ -1,7 +1,16 @@
 package org.project;
 
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ISCTE iscte = new ISCTE();
+        try {
+            iscte.readLeactures("HorarioDeExemplo.csv");
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        iscte.writeDownLectures();
     }
 }
