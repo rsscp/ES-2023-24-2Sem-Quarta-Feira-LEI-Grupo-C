d2b1c4dcb9ec8d808dbab14efc51e63f4589fa61
00Jakub00 <jakubgaly77@gmail.com>
2024-03-21 20:25:46 +0100

    Nothing

--- src/main/java/org/project/Main.java
+++ src/main/java/org/project/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello");
         ISCTE iscte = new ISCTE();
         try {
             iscte.readLeactures("HorarioDeExemplo.csv");
